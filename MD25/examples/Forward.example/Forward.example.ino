876485ba0305eb4ec6f3c63ca6d39ae0fb3882fb
DiefBell <diefenbaker.bell@gmail.com>
2016-11-16 11:19:13 +0000

    Add all initial files.
    Still need to add some debugging, and get it working for multiple addresses.

---
+++ MD25/examples/Forward.example/Forward.example.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include <Enums.MD25.h>
+#include <MD25.h>
+
+/*
+ * This demonstrates the most basic function: going forwards.
+ * 
+ * Use this to calibrate the WheelCircumference value by
+ * moving the robot forwards over a known distance. If it needs
+ * to go further, decrease the first value in the MD25 constructor
+ * and vice verse for overshoot.
+ */
+
+using namespace RD02;
+
+MD25 md25;
+
+void setup() {
+  md25 = MD25(102 * PI, 278/2);  // <-- The MD25 constructor, if in doubt.
+}
+
+void loop() {
+  delay (2000);
+  md25.Forward (1000);
+
+  while (true);
+}
---
+++ MD25/examples/GetBatteryVoltage.example/GetBatteryVoltage.example.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <Enums.MD25.h>
+#include <MD25.h>
+
+/*
+ * Demonstrates use of the GetBatteryVoltage function.
+ * 
+ * This can be used just to check your battery's charge
+ * (given in volts), or is a useful tool for verifying
+ * the I2C connection between Arduino and MD25.
+ */
+
+using namespace RD02;
+MD25 md25;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  md25 = MD25();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  md25.GetBatteryVoltage ();
+  delay (500);
+}
